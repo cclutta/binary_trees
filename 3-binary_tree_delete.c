f835bc20c74b3508040b115261730e4c47a403c2
cclutta <chelsea.lutta@gmail.com>
2022-08-24 20:41:51 +0300

    Update

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,20 +3,18 @@
 /**
 * binary_tree_delete - that deletes an entire binary tree
 * @tree: pointer to root node
+*
 * Return: void
 */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	binary_tree_t *right, *left;
-	if (tree)
+	if (!tree)
 	{
-		if (tree->right || tree->left)
-		{	
-			right = tree->right;
-			left = tree->left;
-			free(right);
-			free(left);
-		}
+		return;
 	}
+	binary_tree_delete(tree->left);
+	binary(tree_delete(tree->right);
+	
+	free(tree);
 }
