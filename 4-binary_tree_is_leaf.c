9feaeade88cb45284602f5d9d1ab903c6aca0783
cclutta <chelsea.lutta@gmail.com>
2022-08-24 20:50:00 +0300

    Update

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_leaf - that checks if a node is a leaf
+* @node: node to check
+*
+* Return: int
+*/
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node)
+	{
+		if (!node->left && !node->right)
+		{
+			return (1);
+		}
+
+	}
+	return (0);
+}
